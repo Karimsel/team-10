0a2852e11b750a90f405e419a6c44dad6f8801c0
Karim <81632937+Karimsel@users.noreply.github.com>
2024-11-06 16:21:21 +0100

    Small changes to property so, Ports don't need to be changed on vm

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ShoppingListView from '../views/ShoppingListView.vue'
 import RecipesView from "@/views/RecipesView.vue";
 import MealPlanView from "@/views/MealPlanView.vue";
 import StatisticsView from "@/views/StatisticsView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
